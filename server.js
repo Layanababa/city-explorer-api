4a05999c4f51579730067ef1641e403ec949620e
LayanaBaba <leana_baba@yahoo.com>
2021-05-24 19:25:25 +0300

    backend

--- server.js
+++ server.js
@@ -2,19 +2,22 @@
 
 require('dotenv').config();
 
-const express =require('express');
-const weatherData=require('./data/weather.json');
-const cors=require('cors');
+const express = require('express');
+const weatherData = require('./data/weather.json');
+const cors = require('cors');
 
 const server = express();
 server.use(cors());
 
-const PORT= process.env.PORT;
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+    console.log(`Listening to PORT ${PORT}`);
+})
 
 // http://localhost:3001/
-server.get('/', (req, res) => {
-    res.send('Home')
-})
+// server.get('/', (req, res) => {
+//     res.send('Home')
+// })
 
 // http://localhost:3001/weather
 // server.get('/weather',(req,res)=>{
@@ -32,19 +35,41 @@ server.get('/', (req, res) => {
 //     // res.send(locationLon);
 // })
 
-server.get('/weather', (request, response) => {
-    let str = 'Hello From back-end!!!';
-    response.status(200).send(str);
+// server.get('/weather', (request, response) => {
+//     let str = 'Hello From back-end!!!';
+//     response.status(200).send(str);
+// })
+// http://localhost:3001/weather?locationName=amman
+server.get('/weather', (req, res) => {
+    let locationNameData = req.query.locationName;
+
+    let requiredLocation = ['amman', 'seattle', 'paris'];
+
+    if (requiredLocation.includes(locationNameData.toLowerCase())) {
+
+        let locationItem = weatherData.find(item => {
+            if (item.city_name.toLowerCase() == locationNameData)
+                
+            return item;
+        })
+        let locationArr = locationItem.data.map(item => {
+            return new Forecast(item);
+        })
+        res.send(locationArr);
+
+    } else {
+        res.send('Not Found!!!');
+    }
+
+
 })
 
-server.get('/weather',(req,res)=>{
-    let locationNameData=req.searchQuery.locationName
-    let locationItem=weatherData.data.find(item=>{
-        if(item.city_name==locationNameData)
-        return item
-    })
-    res.send(locationItem)
-})
+class Forecast {
+    constructor(daily) {
+        this.date = daily.valid_date;
+        this.description = `Low of ${daily.low_temp},high of ${daily.max_temp} with ${daily.weather.description}`
+    }
+}
 
 // server.get('/weather',(req,res)=>{
 //     let locationLatData=req.lat.locationName
@@ -56,9 +81,6 @@ server.get('/weather',(req,res)=>{
 // })
 
 server.get('*', (req, res) => {
-    res.send('Not Found!!!');
+    res.status(200).send('Not Found!!!');
 })
 
-server.listen(PORT,()=>{
-    
-})
